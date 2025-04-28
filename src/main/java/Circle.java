9fd0230624e8ebfc119fef4a83608a7c605cc904
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-28 09:54:39 -0400

    unstatic

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,10 +1,10 @@
 class Circle{
-    static double radius;
+    double radius;
     public Circle(double _radius){
         radius = _radius;
     }
 
-    static double getRadius(){return radius;}
-    static double area(){return 3.14*radius*radius;}
-    static double circumference(){return 3.14*2*radius;}
+    public double getRadius(){return radius;}
+    public double area(){return 3.14*radius*radius;}
+    public double circumference(){return 3.14*2*radius;}
 }
