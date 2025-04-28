2fa2fffd43f803873dc87d386049c2e08cbd36ab
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-28 09:50:03 -0400

    unstatic

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,12 +1,12 @@
 class Rectangle{
-    static double length;
-    static double width;
+    double length;
+    double width;
     public Rectangle(double length_, double width_){
         length = length_;
         width = width_;
     }
-    static double getWidth(){return width;}
-    static double getLength(){return length;}
+    double getWidth(){return width;}
+    double getLength(){return length;}
     double perimeter(){
         if(length > 0 && width > 0){
             return 2*length + 2*width;
