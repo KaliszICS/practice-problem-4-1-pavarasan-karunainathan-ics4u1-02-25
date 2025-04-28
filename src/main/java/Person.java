ae1a4d23657d7c2d30209483678251bd3a77b4cf
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-28 09:53:29 -0400

    unstatic

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,10 +1,10 @@
 class Person{
-    static String name;
-    static int age;
-    static int height;
-    static int weight;
-    static String eyeColour;
-    static String hairColour;
+    String name;
+    int age;
+    int height;
+    int weight;
+    String eyeColour;
+    String hairColour;
 
     public Person(String _name, int _age, int _height, int _weight, String _eyeColour, String _hairColour){
         name = _name;
@@ -15,11 +15,11 @@ class Person{
         hairColour = _hairColour;
     }
 
-    static String getName(){return name;}
-    static int getAge(){return age;}
-    static int getHeight(){return height;}
-    static int getWeight(){return weight;}
-    static String getEyeColour(){return eyeColour;}
-    static String getHairColour(){return hairColour;}
+    public String getName(){return name;}
+    public int getAge(){return age;}
+    public int getHeight(){return height;}
+    public int getWeight(){return weight;}
+    public String getEyeColour(){return eyeColour;}
+    public String getHairColour(){return hairColour;}
 
 }
